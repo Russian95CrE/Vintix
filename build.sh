489bd7f3fb1f2dd03f3492c5b3f28feafb02bbf8
Russian95CrE <russian95@infernointeractive.win>
2025-06-29 00:40:20 -0300

    change: now build.sh asks if you want to fix the file headers

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ clear
 ISO_PATH="build/out/kernel.iso"
 QEMU_COMMAND="qemu-system-x86_64 -cdrom $ISO_PATH -vga vmware -machine hpet=on -m 512M -serial mon:stdio"
 
+# try to fix any missing or incorrect headers
+if ! ./header.sh ; then
+    echo -e "\n\e[33mTried to fix missing or incorrect headers, but failed!\e[0m\n"
+    exit 1
+fi
+
 # try to format the src/ folder
 if ! find src/ -name "*.[ch]" -exec clang-format -i {} +; then
     echo -e "\n\e[33mTried to format src/ folder using .clang-format, but failed!\e[0m\n"
